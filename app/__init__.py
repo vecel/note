36787d1a550ba5639a6ef57a15cb1d853f884bd5
vecel <mateusz.karandys@gmail.com>
2025-05-11 19:25:38 +0200

    Init command

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,4 @@
 __app_name__ = "Notes"
 __version__ = "0.1.0"
+
+NOTES_FILENAME = ".notes"
--- app/commands/init.py
+++ app/commands/init.py
@@ -5,11 +5,22 @@ This module defines the `init` command which prepares the environment
 for storing notes. It typically creates a `.notes` file in the current 
 directory.
 """
+import os
 
 import typer
+from typing_extensions import Annotated
+
+from app import NOTES_FILENAME
 
 app = typer.Typer()
 
 @app.command()
 def init():
-    print("Initializing .notes file.")
+    """
+    Initializes empty notes repository in working directory.
+    """
+    try:
+        with open(".notes", "x") as _:
+            print("Initialized empty notes repository.")
+    except FileExistsError:
+        print(f"Notes repository already initialized in {os.getcwd()}/{NOTES_FILENAME}")
