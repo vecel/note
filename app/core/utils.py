f30b2498899302e9944f8f99a82223f90dda895a
vecel <mateusz.karandys@gmail.com>
2025-05-19 12:25:50 +0200

    Change Exception to NoteAppError

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,7 +1,11 @@
 import re
 
+from app.core.errors import NoteAppError
+
 def parse_tags(tags: str):
     result = re.match("^[a-zA-Z0-9]+(,[a-zA-Z0-9]+)*$", tags)
     if result is None:
-        raise Exception("Invalid tags format.") # TODO change to custom exception
+        raise NoteAppError("Note was not added because of an error. Tags " \
+        "should be comma separated list of strings containing only " \
+        "letters and numbers.") # TODO change to custom exception
     return tags.split(",")
