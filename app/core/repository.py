7e51eda3f5d4c0db98fbe2ad389e9b6fa574d800
vecel <mateusz.karandys@gmail.com>
2025-05-29 18:39:40 +0200

    Refactor repository.py, create Repository class

--- app/core/repository.py
+++ app/core/repository.py
@@ -2,152 +2,174 @@
 Provides high-level operations on the note repository, such as creating the repository
 and adding notes. This module separates core logic from low-level file storage operations.
 """
+from copy import deepcopy
+
 from app.core import storage
 from app.core.models import Note, Status, NoteWithStatus
 from app.core.errors import RepositoryCorruptedError, NotesNotFoundError, NoteAppError, StatusDoesNotExistError
 from app.core.utils import print_notes, print_tags, print_statuses
 
-def create_repository():
-    """
-    Initializes a new note repository by delegating to the storage layer.
+class Repository:
+    def __enter__(self):
+        self.repository = storage.load_repository()
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        storage.save_repository(self.repository)
 
-    Returns:
-        Path: The absolute path to the newly created repository file.
-    """
-    return storage.create_repository()
+    @property
+    def _notes(self):
+        if "notes" not in self.repository.keys():
+            raise RepositoryCorruptedError("Notes repository does not contain field 'notes'.")
+        return self.repository["notes"]
+    
+    @property
+    def _statuses(self):
+        if "config" not in self.repository.keys():
+            raise RepositoryCorruptedError("Notes repository does not contain field 'config'.")
+        
+        if "statuses" not in self.repository["config"].keys():
+            raise RepositoryCorruptedError("Notes repository configuration does not contain field 'statuses'.")
+        
+        return self.repository["config"]["statuses"]
+
+    @property
+    def _indexed_notes(self):
+        return [
+            NoteWithStatus(
+                idx, 
+                Note(**note), 
+                Status(**self._statuses[note["status"]]) if note["status"] else Status.create()
+            ) 
+            for idx, note in enumerate(self._notes)
+        ]
+
+    @property
+    def _notes_sorter(self):
+        return lambda note_dict: 0 if note_dict["status"] is None else -self._statuses[note_dict["status"]]["priority"]
+
+    @staticmethod
+    def init_repository():
+        """
+        Initializes a new note repository by delegating to the storage layer.
+
+        Returns:
+            Path: The absolute path to the newly created repository file.
+        """
+        return storage.create_repository()
+
+    def add_note(self, note: Note):
+        """
+        Adds a new note to the notes repository.
+
+        This function loads the existing repository, verifies that it contains a
+        'notes' field, appends the new note, and saves the updated repository 
+        back to disk.
+
+        Args:
+            note (Note): The Note object to be added to the repository.
+
+        Raises:
+            RepositoryCorruptedError: If the loaded repository does not contain a 'notes' field,
+                                    indicating it is improperly structured or corrupted.
+            StatusDoesNotExistErrorl: If note is being created with non-existing status.
+        """
+        if note.status and note.status not in self._statuses.keys():
+            raise StatusDoesNotExistError(f"There is no status {note.status} in the repository configuration. Run `note list -S` to see all statuses or `note status --add STATUS` to add a new one.")
+
+        self._notes.append(note.to_dict())
+        self._notes.sort(key=self._notes_sorter)
+
+    def list_notes(self, tag_filter: list[str] | None = None):
+        if not self._indexed_notes:
+            raise NotesNotFoundError("Repository is empty. Run `note add` to add a note.")
+        
+        filtered_notes = deepcopy(self._indexed_notes)
+        if tag_filter:
+            filtered_notes = [inote for inote in filtered_notes if inote.note.tags and set(inote.note.tags) & set(tag_filter)]
+            if not filtered_notes:
+                filter_msg = ", ".join(tag_filter)
+                raise NotesNotFoundError(f"There are no notes matching filter: '{filter_msg}' in repository.")
+        
+        print_notes(filtered_notes)
+
+    def list_tags(self):
+        tags = [note["tags"] for note in self._notes if note["tags"]]
+        tags = [tag for group in tags for tag in group]
+        if not tags:
+            raise NotesNotFoundError("There are no tagged notes in the repository.")
+        print_tags(tags)
+
+    def list_statuses(self):
+        statuses = [(name, Status.create(**status)) for name, status in self._statuses.items()]
+        if not statuses:
+            raise NoteAppError("There are no statuses in repository configuration. Run `note status --add` to create one.") # TODO test for that
+        print_statuses(statuses)
+
+    def delete_note(self, idx: int):
+        if not 1 <= idx <= len(self._notes):
+            raise NotesNotFoundError(f"There is no note with id {idx} in the repository. Run `note list` to see all notes.")
+        self._notes.pop(idx - 1)
+
+    def create_status(self, name: str, status: Status):
+        if name in self._statuses.keys():
+            raise NoteAppError(f"Status {name} already exists. Use `note config status -e` to edit statuses.")
+        
+        self._statuses[name] = {
+            "style": status.style,
+            "priority": status.priority
+        } 
+    
+    def edit_status(self, name: str, status: Status):
+        if name not in self._statuses.keys():
+            raise StatusDoesNotExistError(f"There is no status {name} in the repository configuration. Run `note list -S` to see all statuses or `note status --add STATUS` to add a new one.")
+        
+        self._statuses[name] = {
+            "style": status.style,
+            "priority": status.priority
+        }
+
+        self._notes.sort(key=self._notes_sorter)
+
+    def delete_status(self, name: str):
+        if name not in self._statuses.keys():
+            raise StatusDoesNotExistError(f"There is no status {name} in the repository configuration. Run `note list -S` to see all statuses or `note status --add STATUS` to add a new one.")
+        # TODO check whether there are notes with this status, if so ask user what to do or raise an exception
+        self._statuses.pop(name)
+
+
+
+def create_repository():
+    return Repository.init_repository()
 
 def add_note(note: Note):
-    """
-    Adds a new note to the notes repository.
-
-    This function loads the existing repository, verifies that it contains a
-    'notes' field, appends the new note, and saves the updated repository 
-    back to disk.
-
-    Args:
-        note (Note): The Note object to be added to the repository.
-
-    Raises:
-        RepositoryCorruptedError: If the loaded repository does not contain a 'notes' field,
-                                  indicating it is improperly structured or corrupted.
-        StatusDoesNotExistErrorl: If note is being created with non-existing status.
-    """
-    repository = storage.load_repository()
-    notes = _get_notes(repository)
-    statuses = _get_statuses(repository)
-    
-    if note.status and note.status not in statuses.keys():
-        raise StatusDoesNotExistError(f"There is no status {note.status} in the repository configuration. Run `note list -S` to see all statuses or `note status --add STATUS` to add a new one.")
-
-    notes.append(note.to_dict())
-
-    _sort_notes(repository)
-
-    storage.save_repository(repository)
+    with Repository() as repo:
+        repo.add_note(note)
 
 def list_notes(tag_filter: list[str] | None = None):
-    repository = storage.load_repository()
-    notes = _get_notes_with_statuses(repository)
-
-    if not notes:
-        raise NotesNotFoundError("Repository is empty. Run `note add` to add a note.")
-    
-    if tag_filter is not None:
-        notes = [note for note in notes if note.note.tags and set(note.note.tags) & set(tag_filter)]
-        if not notes:
-            filter_msg = ", ".join(tag_filter)
-            raise NotesNotFoundError(f"There are no notes matching filter: '{filter_msg}' in repository.")
-    
-    print_notes(notes)
+    with Repository() as repo:
+        repo.list_notes(tag_filter)
 
 def list_tags():
-    repository = storage.load_repository()
-    notes = _get_notes(repository)
-    tags = [note["tags"] for note in notes if note["tags"]]
-    tags = [tag for group in tags for tag in group]
-    if not tags:
-        raise NotesNotFoundError("There are no tagged notes in the repository.")
-    print_tags(tags)
+    with Repository() as repo:
+        repo.list_tags()
 
 def list_statuses():
-    repository = storage.load_repository()
-    statuses = _get_statuses(repository)
-    statuses = [(name, Status.create(**status)) for name, status in statuses.items()]
-    if not statuses:
-        raise NoteAppError("There are no statuses in repository configuration. Run `note status --add` to create one.") # TODO test for that
-    print_statuses(statuses)
+    with Repository() as repo:
+        repo.list_statuses()
 
-def delete_note(id: int):
-    repository = storage.load_repository()
-    notes = _get_notes(repository)
-    if not 1 <= id <= len(notes):
-        raise NotesNotFoundError(f"There is no note with id {id} in the repository. Run `note list` to see all notes.")
-    notes.pop(id - 1)
-    storage.save_repository(repository)
+def delete_note(idx: int):
+    with Repository() as repo:
+        repo.delete_note(idx)
 
 def create_status(name: str, status: Status):
-    repository = storage.load_repository()
-    statuses = _get_statuses(repository)
-
-    if name in statuses.keys():
-        raise NoteAppError(f"Status {name} already exists. Use `note config status -e` to edit statuses.")
-    
-    statuses[name] = {
-        "style": status.style,
-        "priority": status.priority
-    }
-    storage.save_repository(repository)
+    with Repository() as repo:
+        repo.create_status(name, status)
 
 def edit_status(name: str, status: Status):
-    repository = storage.load_repository()
-    statuses = _get_statuses(repository)
-
-    if name not in statuses.keys():
-        raise StatusDoesNotExistError(f"There is no status {name} in the repository configuration. Run `note list -S` to see all statuses or `note status --add STATUS` to add a new one.")
-    
-    statuses[name] = {
-        "style": status.style,
-        "priority": status.priority
-    }
-
-    _sort_notes(repository)
-
-    storage.save_repository(repository)
+    with Repository() as repo:
+        repo.edit_status(name, status)
 
 def delete_status(name: str):
-    repository = storage.load_repository()
-    statuses = _get_statuses(repository)
-
-    if name not in statuses.keys():
-        raise StatusDoesNotExistError(f"There is no status {name} in the repository configuration. Run `note list -S` to see all statuses or `note status --add STATUS` to add a new one.")
-    
-    # TODO check whether there are notes with this status, if so ask user what to do or raise an exception
-
-    statuses.pop(name)
-    storage.save_repository(repository)
-
-def _get_notes(repository: dict):
-    if "notes" not in repository.keys():
-        raise RepositoryCorruptedError("Notes repository does not contain field 'notes'.")
-    return repository["notes"]
-
-def _get_statuses(repository: dict):
-    if "config" not in repository.keys():
-        raise RepositoryCorruptedError("Notes repository does not contain field 'config'.")
-    
-    if "statuses" not in repository["config"].keys():
-        raise RepositoryCorruptedError("Notes repository configuration does not contain field 'statuses'.")
-    
-    return repository["config"]["statuses"]
-
-def _get_notes_with_statuses(repository: dict):
-    notes = _get_notes(repository)
-    statuses = _get_statuses(repository)
-
-    return [NoteWithStatus(idx, Note(**note), Status(**statuses[note["status"]]) if note["status"] else Status.create()) for idx, note in enumerate(notes)]
-
-def _sort_notes(repository: dict):
-    notes = _get_notes(repository)
-    statuses = _get_statuses(repository)
-
-    notes.sort(key=lambda note_dict: 0 if note_dict["status"] is None else statuses[note_dict["status"]]["priority"], reverse=True)
+    with Repository() as repo:
+        repo.delete_status(name)
