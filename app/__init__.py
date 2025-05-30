eebd85041d3983b13cb392acde98678a24c7b49b
vecel <mateusz.karandys@gmail.com>
2025-05-30 17:57:53 +0200

    Add init test

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
 __app_name__ = "notecli"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
--- tests/init_test.py
+++ tests/init_test.py
@@ -1,11 +1,11 @@
 import os
+import json
 
 from app.core import REPOSITORY_FILENAME
 
 def test_init(tmp_path, runner, test_app):
     os.chdir(tmp_path)
     notes_file = tmp_path / REPOSITORY_FILENAME
-    
     result = runner.invoke(test_app, ["init"])
 
     assert result.exit_code == 0
@@ -18,3 +18,18 @@ def test_init_already_exists(repo_initialized, runner, test_app):
     assert result.exit_code == 0
     assert repo_initialized.exists()
     assert f"Notes repository already initialized in {repo_initialized}" in result.stdout
+
+
+def test_init_repository_structure(tmp_path, runner, test_app):
+    os.chdir(tmp_path)
+    notes_file = tmp_path / REPOSITORY_FILENAME
+    result = runner.invoke(test_app, ["init"])
+
+    with open(notes_file, "r") as file:
+        repository = json.load(file)
+
+    assert result.exit_code == 0
+    assert isinstance(repository, dict)
+    assert "notes" in repository.keys()
+    assert "config" in repository.keys()
+    assert "statuses" in repository["config"].keys()
